074fe044b8041c2197d010993f384b47d7bc1353
Nontikweed <39436372+nontikweed@users.noreply.github.com>
2026-05-19 17:26:14 +0800

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -300,7 +300,7 @@ echo -e "[\e[32mInfo\e[0m] Installation SquidProxy Complete."
 sleep 5
 }
 
-install_openvpn()
+install_openvpn1()
 {
 echo -n -e "[\e[32mInfo\e[0m]" && echo -e " Installing OpenVPN Server." | lolcat
 if [[ ! -e /etc/openvpn ]]; then
@@ -571,6 +571,277 @@ n/406HSgtsB8yWPDNga/N7OONk8aTJtbWwIBAg==
 -----END DH PARAMETERS-----
 EOF
 
+chmod 644 /etc/openvpn/server/*
+chmod 600 /etc/openvpn/server/server.key
+chmod 755 /etc/openvpn/
+cat << 'EOF' > /etc/openvpn/auth.sh
+#!/bin/bash
+
+response=$(curl --connect-timeout 10 --max-time 15 -s -X POST \
+-H "X-API-KEY: xebecc" \
+-d "username=$username" \
+-d "password=$password" \
+https://walanakongmaisip.info/api/auth)
+
+if [[ "$response" == "OK" ]]; then
+    exit 0
+else
+    exit 1
+fi
+EOF
+
+chmod +x /etc/openvpn/auth.sh
+chmod 755 /etc/openvpn/server_udp.conf
+chmod 755 /etc/openvpn/server_tcp.conf
+
+modprobe tun
+mkdir -p /dev/net
+
+echo -n -e "[\e[33mNotice\e[0m]" && echo -e " Enabling and starting OpenVPN UDP." | lolcat
+sudo systemctl enable openvpn-server@server_udp.service > /dev/null 2>&1
+sudo systemctl start openvpn-server@server_udp.service > /dev/null 2>&1
+
+echo -n -e "[\e[33mNotice\e[0m]" && echo -e " Enabling and starting OpenVPN TCP." | lolcat
+sudo systemctl enable openvpn-server@server_tcp.service > /dev/null 2>&1
+sudo systemctl start openvpn-server@server_tcp.service > /dev/null 2>&1
+ 
+ }
+echo -n -e "[\e[32mInfo\e[0m]" && echo -e " Installing OpenVPN Complete." | lolcat
+reset
+}
+
+install_openvpn2()
+{
+echo -n -e "[\e[32mInfo\e[0m]" && echo -e " Installing OpenVPN Server." | lolcat
+if [[ ! -e /etc/openvpn ]]; then
+ mkdir -p /etc/openvpn
+ else
+ if [ ! -e /etc/openvpn.bak.popotworks ]; then
+  cp -a /etc/openvpn /etc/openvpn.bak.popotworks
+ fi
+ rm -rf /etc/openvpn/*
+fi
+{
+
+mkdir -p /etc/openvpn/server
+
+echo 'DNS=1.1.1.1
+DNSStubListener=no' >> /etc/systemd/resolved.conf > /dev/null 2>&1 
+
+echo '# Openvpn Configuration by Kobe Kobz :)
+dev tun
+port 110
+proto udp
+topology subnet
+server 10.30.0.0 255.255.252.0
+ca /etc/openvpn/server/ca.crt
+cert /etc/openvpn/server/server.crt
+key /etc/openvpn/server/server.key
+dh none
+tls-server
+tls-version-min 1.2
+tls-cipher TLS-ECDHE-RSA-WITH-AES-128-GCM-SHA256
+cipher none
+ncp-disable
+auth none
+sndbuf 0
+rcvbuf 0
+keepalive 10 180
+persist-key
+persist-tun
+ping-timer-rem
+reneg-sec 0
+user nobody
+group nogroup
+client-to-client
+username-as-common-name
+verify-client-cert none
+client-cert-not-required
+script-security 3
+duplicate-cn
+auth-user-pass-verify /etc/openvpn/auth.sh via-env
+push "persist-key"
+push "persist-tun"
+push "dhcp-option DNS 1.1.1.1"
+push "dhcp-option DNS 1.0.0.1"
+push "redirect-gateway def1 bypass-dhcp"
+push "sndbuf 0"
+push "rcvbuf 0"
+log /etc/openvpn/server/udpserver.log
+status /etc/openvpn/server/udpclient.log
+ifconfig-pool-persist udpip.txt
+status-version 2
+verb 3' > /etc/openvpn/server_udp.conf
+
+echo '# Openvpn Configuration by Kobe Kobz :)
+dev tun
+port 1194
+proto tcp
+topology subnet
+server 10.20.0.0 255.255.252.0
+ca /etc/openvpn/server/ca.crt
+cert /etc/openvpn/server/server.crt
+key /etc/openvpn/server/server.key
+dh none
+tls-server
+tls-version-min 1.2
+tls-cipher TLS-ECDHE-RSA-WITH-AES-128-GCM-SHA256
+cipher none
+ncp-disable
+auth none
+socket-flags TCP_NODELAY
+sndbuf 0
+rcvbuf 0
+keepalive 10 120
+persist-key
+persist-tun
+ping-timer-rem
+reneg-sec 0
+user nobody
+group nogroup
+client-to-client
+username-as-common-name
+verify-client-cert none
+client-cert-not-required
+script-security 3
+duplicate-cn
+auth-user-pass-verify /etc/openvpn/radius/authme via-env
+push "socket-flags TCP_NODELAY"
+push "persist-key"
+push "persist-tun"
+push "dhcp-option DNS 8.8.8.8"
+push "dhcp-option DNS 8.8.4.4"
+push "redirect-gateway def1 bypass-dhcp"
+push "sndbuf 0"
+push "rcvbuf 0"
+log /etc/openvpn/server/tcpserver.log
+status /etc/openvpn/server/tcpclient.log
+ifconfig-pool-persist /etc/openvpn/server/tcpip.txt
+status-version 2
+verb 3
+max-clients 450' > /etc/openvpn/server_tcp.conf
+
+sed -i "s|PORT_TCP|$PORT_TCP|g" /etc/openvpn/server_tcp.conf
+cp /etc/openvpn/server_udp.conf /etc/openvpn/server/server_udp.conf
+cp /etc/openvpn/server_tcp.conf /etc/openvpn/server/server_tcp.conf
+
+cat << EOF > /etc/openvpn/server/ca.crt
+-----BEGIN CERTIFICATE-----
+MIICMTCCAZqgAwIBAgIUAaQBApMS2dYBqYPcA3Pa7cjjw7cwDQYJKoZIhvcNAQEL
+BQAwDzENMAsGA1UEAwwES29iWjAeFw0yMDA3MjIyMjIzMzNaFw0zMDA3MjAyMjIz
+MzNaMA8xDTALBgNVBAMMBEtvYlowgZ8wDQYJKoZIhvcNAQEBBQADgY0AMIGJAoGB
+AMF46UVi2O5pZpddOPyzU2EyIrr8NrpXqs8BlYhUjxOcCrkMjFu2G9hk7QIZ4qO0
+GWVZpPhYk5qWk+LxCsryrSoe0a5HaqIye8BFJmXV0k+O/3e6k06UGNii3gxBWQpF
+7r/2CyQLus9OSpQPYszByBvtkwiBAo/V98jdpm+EVu6tAgMBAAGjgYkwgYYwHQYD
+VR0OBBYEFGRJMm/+ZmLxV027kahdvSY+UaTSMEoGA1UdIwRDMEGAFGRJMm/+ZmLx
+V027kahdvSY+UaTSoROkETAPMQ0wCwYDVQQDDARLb2JaghQBpAECkxLZ1gGpg9wD
+c9rtyOPDtzAMBgNVHRMEBTADAQH/MAsGA1UdDwQEAwIBBjANBgkqhkiG9w0BAQsF
+AAOBgQC0f8wb5hyEOEEX64l8QCNpyd/WLjoeE5bE+xnIcKE+XpEoDRZwugLoyQdc
+HKa3aRHNqKpR7H696XJReo4+pocDeyj7rATbO5dZmSMNmMzbsjQeXux0XjwmZIHu
+eDKMefDi0ZfiZmnU2njmTncyZKxv18Ikjws0Myc8PtAxy2qdcA==
+-----END CERTIFICATE-----
+EOF
+
+cat << EOF > /etc/openvpn/server/server.crt
+Certificate:
+    Data:
+        Version: 3 (0x2)
+        Serial Number:
+            40:26:da:91:18:2b:77:9c:85:6a:0c:bb:ca:90:53:fe
+        Signature Algorithm: sha256WithRSAEncryption
+        Issuer: CN=KobZ
+        Validity
+            Not Before: Jul 22 22:23:55 2020 GMT
+            Not After : Jul 20 22:23:55 2030 GMT
+        Subject: CN=server
+        Subject Public Key Info:
+            Public Key Algorithm: rsaEncryption
+                RSA Public-Key: (1024 bit)
+                Modulus:
+                    00:ce:35:23:d8:5d:9f:b6:9b:cb:6a:89:e1:90:af:
+                    42:df:5f:f8:bd:ad:a7:78:9a:ca:20:f0:3d:5b:d6:
+                    c9:ef:4c:4a:99:96:c3:38:fd:59:b4:d7:65:ed:d4:
+                    a7:fa:ab:03:e2:be:88:2f:ca:fc:90:dd:b0:b7:bc:
+                    23:cb:83:ac:36:e2:01:57:69:64:b8:e1:9e:51:f0:
+                    a6:9d:13:d9:92:6b:4d:04:a6:10:64:a3:3f:6b:ff:
+                    fe:32:ac:91:63:c2:71:24:be:9e:76:4f:87:cc:3a:
+                    03:a1:9e:48:3f:11:92:33:3b:19:16:9c:d0:5d:16:
+                    ee:c1:42:67:99:47:66:67:67
+                Exponent: 65537 (0x10001)
+        X509v3 extensions:
+            X509v3 Basic Constraints: 
+                CA:FALSE
+            X509v3 Subject Key Identifier: 
+                6B:08:C0:64:10:71:A8:32:7F:0B:FE:1E:98:1F:BD:72:74:0F:C8:66
+            X509v3 Authority Key Identifier: 
+                keyid:64:49:32:6F:FE:66:62:F1:57:4D:BB:91:A8:5D:BD:26:3E:51:A4:D2
+                DirName:/CN=KobZ
+                serial:01:A4:01:02:93:12:D9:D6:01:A9:83:DC:03:73:DA:ED:C8:E3:C3:B7
+
+            X509v3 Extended Key Usage: 
+                TLS Web Server Authentication
+            X509v3 Key Usage: 
+                Digital Signature, Key Encipherment
+            X509v3 Subject Alternative Name: 
+                DNS:server
+    Signature Algorithm: sha256WithRSAEncryption
+         a1:3e:ac:83:0b:e5:5d:ca:36:b7:d0:ab:d0:d9:73:66:d1:62:
+         88:ce:3d:47:9e:08:0b:a0:5b:51:13:fc:7e:d7:6e:17:0e:bd:
+         f5:d9:a9:d9:06:78:52:88:5a:e5:df:d3:32:22:4a:4b:08:6f:
+         b1:22:80:4f:19:d1:5f:9d:b6:5a:17:f7:ad:70:a9:04:00:ff:
+         fe:84:aa:e1:cb:0e:74:c0:1a:75:0b:3e:98:90:1d:22:ba:a4:
+         7a:26:65:7d:d1:3b:5c:45:a1:77:22:ed:b6:6b:18:a3:c4:ee:
+         3e:06:bb:0b:ec:12:ac:16:a5:50:b3:ed:46:43:87:72:fd:75:
+         8c:38
+-----BEGIN CERTIFICATE-----
+MIICVDCCAb2gAwIBAgIQQCbakRgrd5yFagy7ypBT/jANBgkqhkiG9w0BAQsFADAP
+MQ0wCwYDVQQDDARLb2JaMB4XDTIwMDcyMjIyMjM1NVoXDTMwMDcyMDIyMjM1NVow
+ETEPMA0GA1UEAwwGc2VydmVyMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDO
+NSPYXZ+2m8tqieGQr0LfX/i9rad4msog8D1b1snvTEqZlsM4/Vm012Xt1Kf6qwPi
+vogvyvyQ3bC3vCPLg6w24gFXaWS44Z5R8KadE9mSa00EphBkoz9r//4yrJFjwnEk
+vp52T4fMOgOhnkg/EZIzOxkWnNBdFu7BQmeZR2ZnZwIDAQABo4GuMIGrMAkGA1Ud
+EwQCMAAwHQYDVR0OBBYEFGsIwGQQcagyfwv+HpgfvXJ0D8hmMEoGA1UdIwRDMEGA
+FGRJMm/+ZmLxV027kahdvSY+UaTSoROkETAPMQ0wCwYDVQQDDARLb2JaghQBpAEC
+kxLZ1gGpg9wDc9rtyOPDtzATBgNVHSUEDDAKBggrBgEFBQcDATALBgNVHQ8EBAMC
+BaAwEQYDVR0RBAowCIIGc2VydmVyMA0GCSqGSIb3DQEBCwUAA4GBAKE+rIML5V3K
+NrfQq9DZc2bRYojOPUeeCAugW1ET/H7XbhcOvfXZqdkGeFKIWuXf0zIiSksIb7Ei
+gE8Z0V+dtloX961wqQQA//6EquHLDnTAGnULPpiQHSK6pHomZX3RO1xFoXci7bZr
+GKPE7j4GuwvsEqwWpVCz7UZDh3L9dYw4
+-----END CERTIFICATE-----
+
+EOF
+
+cat << EOF > /etc/openvpn/server/server.key
+-----BEGIN PRIVATE KEY-----
+MIICdQIBADANBgkqhkiG9w0BAQEFAASCAl8wggJbAgEAAoGBAM41I9hdn7aby2qJ
+4ZCvQt9f+L2tp3iayiDwPVvWye9MSpmWwzj9WbTXZe3Up/qrA+K+iC/K/JDdsLe8
+I8uDrDbiAVdpZLjhnlHwpp0T2ZJrTQSmEGSjP2v//jKskWPCcSS+nnZPh8w6A6Ge
+SD8RkjM7GRac0F0W7sFCZ5lHZmdnAgMBAAECgYAFNrC+UresDUpaWjwaxWOidDG8
+0fwu/3Lm3Ewg21BlvX8RXQ94jGdNPDj2h27r1pEVlY2p767tFr3WF2qsRZsACJpI
+qO1BaSbmhek6H++Fw3M4Y/YY+JD+t1eEBjJMa+DR5i8Vx3AE8XOdTXmkl/xK4jaB
+EmLYA7POyK+xaDCeEQJBAPJadiYd3k9OeOaOMIX+StCs9OIMniRz+090AJZK4CMd
+jiOJv0mbRy945D/TkcqoFhhScrke9qhgZbgFj11VbDkCQQDZ0aKBPiZdvDMjx8WE
+y7jaltEDINTCxzmjEBZSeqNr14/2PG0X4GkBL6AAOLjEYgXiIvwfpoYE6IIWl3re
+ebCfAkAHxPimrixzVGux0HsjwIw7dl//YzIqrwEugeSG7O2Ukpz87KySOoUks3Z1
+yV2SJqNWskX1Q1Xa/gQkyyDWeCeZAkAbyDBI+ctc8082hhl8WZunTcs08fARM+X3
+FWszc+76J1F2X7iubfIWs6Ndw95VNgd4E2xDATNg1uMYzJNgYvcTAkBoE8o3rKkp
+em2n0WtGh6uXI9IC29tTQGr3jtxLckN/l9KsJ4gabbeKNoes74zdena1tRdfGqUG
+JQbf7qSE3mg2
+-----END PRIVATE KEY-----
+
+EOF
+
+cat << EOF > /etc/openvpn/server/dh.pem
+-----BEGIN DH PARAMETERS-----
+MIIBCAKCAQEA14VaIGRxzoAjo28NiVCBAJNK9c49hweMSg1HcLo3eoudjZ82cqf9
+BYkCBhfWFIiOu0DbUJrbEJGlmeoZ4pmOE2153GpIkrobRmpZZykaB2h5ymXEGEPV
+nRmve2J5UqJ/VQA6GUskIlGthc1370MgsZ/HBaoJSCa1zqGRB92D1R2TiK2oBH5h
++tiW6Y7sOz3QXAV5tfCksqw3gZalb4Ol6hVfTPgeEH2XrgOgG4Zxpajn0KCJVUSF
+wd7H6pMnFzU3p6TQoxr9myntkPoo+0HHuyW2GE8sbnZPLOvurXcvchkwz+vBX4Rl
+bwprMqHounF1ZATclFUdzIxukXtXW7NeMwIBAg==
+-----END DH PARAMETERS-----
+EOF
+
 chmod 644 /etc/openvpn/nontikweed/*
 chmod 600 /etc/openvpn/nontikweed/server.key
 chmod 755 /etc/openvpn/
@@ -609,7 +880,6 @@ sudo systemctl start openvpn-server@server_tcp.service > /dev/null 2>&1
 echo -n -e "[\e[32mInfo\e[0m]" && echo -e " Installing OpenVPN Complete." | lolcat
 reset
 }
-
 
 install_firewall_kvm () {
 
@@ -997,7 +1267,17 @@ install_dependencies
 install_ssh
 install_dropbear
 install_squid
-install_openvpn
+if [[ "$OPENVPN_VERSION" == "ovpn1" ]]; then
+
+    install_openvpn1
+
+fi
+
+if [[ "$OPENVPN_VERSION" == "ovpn2" ]]; then
+
+    install_openvpn2
+
+fi
 install_stunnel
 install_badvpn
 install_slowdns
